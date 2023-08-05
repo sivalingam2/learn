39f76bd3162ba69d36b85272d87a8e79ccd59f77
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-05 15:53:58 +0530

    exit status

--- varibale.sh
+++ varibale.sh
@@ -4,19 +4,19 @@ echo a is ${a}
 #special variables
 #$0-$N,$#,$*
 
-first argument - $1
-second argument - $2
-no of arguments - $#
-total arguments - $*
+echo first1 argument - $1
+echo second1 argument - $2
+echo noof1 arguments - $#
+echo total1 arguments - $*
 
 #command substitution
  DTAE= $(date)
 
- echo today date is ${DATE}
+ echo "today date is ${DATE}"
  # arithmetic substitution
  ADD=$(( 2+2 ))
 
- echo  2+2 is = $ADD
+ echo  "2+2 is = $ADD"
 
 
 
