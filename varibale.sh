f8296592c5144ff77eaa269be87e529f4b979ca4
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-05 12:28:14 +0530

    exit status

--- varibale.sh
+++ varibale.sh
@@ -4,10 +4,10 @@ echo a is ${a}
 #special variables
 #$0-$N,$#,$*
 
-first argument = $1
-second argument = $2
-no of arguments = $#
-total arguments = $*
+first argument=$1
+second argument=$2
+no of arguments=$#
+total arguments=$*
 
 #command substitution
  DTAE= $(date)
