b24033a3a91f322a684567f22fc52d90bba71040
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-05 11:33:08 +0530

    exit status

---
+++ varibale.sh
@@ -0,0 +1,22 @@
+a=10
+ech a is ${a}
+
+#special variables
+#$0-$N,$#,$*
+
+first argument = $1
+second argument = $2
+no of arguments = $#
+total arguments = $*
+
+#command substitution
+ DTAE= $(date)
+
+ echo today date is $DATE
+# arithmetic substitution
+ ADD=$(( 2+2 ))
+
+ echo  2+2 is = $ADD
+
+
+
